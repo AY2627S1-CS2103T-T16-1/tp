200d50555843633b7245cae20fdcee442cd7453d
jxinnan <jiangxinnan416@gmail.com>
2026-09-18 17:11:21 +0800

    Parse remark command arguments

---
+++ src/test/java/seedu/address/logic/parser/RemarkCommandParserTest.java
@@ -0,0 +1,32 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.RemarkCommand;
+
+public class RemarkCommandParserTest {
+
+    private final RemarkCommandParser parser = new RemarkCommandParser();
+
+    @Test
+    public void parse_validArgs_returnsRemarkCommand() {
+        assertParseSuccess(parser, "1 r/Some remark",
+                new RemarkCommand(INDEX_FIRST_PERSON, "Some remark"));
+    }
+
+    @Test
+    public void parse_missingRemark_returnsRemarkCommandWithEmptyRemark() {
+        assertParseSuccess(parser, "1", new RemarkCommand(INDEX_FIRST_PERSON, ""));
+    }
+
+    @Test
+    public void parse_invalidIndex_throwsParseException() {
+        assertParseFailure(parser, "a r/Some remark",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, RemarkCommand.MESSAGE_USAGE));
+    }
+}
